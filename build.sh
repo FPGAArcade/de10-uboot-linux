9e79e69ef86a19bd287b09eac50a2b7068d90de5
kkimber <45757615+kkimber@users.noreply.github.com>
2021-09-17 22:58:14 +0100

    Initial sources for DE10 u-boot

---
+++ build.sh
@@ -0,0 +1,28 @@
+wget https://developer.arm.com/-/media/Files/downloads/gnu-a/10.2-2020.11/binrel/\
+	gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
+tar xf gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
+export PATH=`pwd`/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH
+
+git clone replay_de10 replay_de10
+
+cd replay_de10
+
+export TOP_FOLDER=`pwd`
+
+# TODO regenerate BSP if QSYS changes ????
+
+cd $TOP_FOLDER/replay_ghrd/software/bootloader
+
+git clone https://github.com/altera-opensource/u-boot-socfpga
+cd u-boot-socfpga
+
+git checkout -b replay-bootloader -t origin/socfpga_v2021.01
+
+./arch/arm/mach-socfpga/qts-filter.sh cyclone5 ../../../ ../ ./board/terasic/de10-nano/qts/
+
+export CROSS_COMPILE=arm-none-linux-gnueabihf-
+make socfpga_de10_nano_defconfig
+make -j 48
+
+
+
