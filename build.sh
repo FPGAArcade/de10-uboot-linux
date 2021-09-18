e1b0aa0e7bea4b7d14142493c7e49bc400a662a5
Erik Hemming <eriquesnk@gmail.com>
2021-09-18 21:52:19 +0200

    Remove WSL refs and download/cache cross-gcc

--- build.sh
+++ build.sh
@@ -1,11 +1,36 @@
-#wget https://developer.arm.com/-/media/Files/downloads/gnu-a/10.2-2020.11/binrel/\
-#	gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
-#tar xf gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
-cd ~
-export PATH=`pwd`/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH
-cd /mnt/c/replay/de10-uboot-linux
+#!/bin/bash
 
-export TOP_FOLDER=`pwd`
+# apt install bison flex
+
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $REPO_ROOT
+
+GCC_PREFIX=arm-none-linux-gnueabihf-
+
+# check if we already have GCC
+if ! hash ${GCC_PREFIX}gcc 2>/dev/null; then
+    CROSS_GCC_ROOT=$HOME/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+
+    echo "'${GCC_PREFIX}gcc' not found; is it in '${CROSS_GCC_ROOT}/' .. ?"
+
+    if [ -e $CROSS_GCC_ROOT/bin/${GCC_PREFIX}gcc ]; then
+        echo "Yep! All good!"
+        export PATH=$CROSS_GCC_ROOT/bin:$PATH
+    else
+        echo "Nope; Let's download it!"
+        wget https://developer.arm.com/-/media/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
+        echo "Extracting..."
+        tar xf gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz -C $HOME
+        export PATH=$CROSS_GCC_ROOT/bin:$PATH
+        rm gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
+        echo "OK! All good!"
+    fi
+fi
+
+${GCC_PREFIX}gcc --version
+
+
+export TOP_FOLDER=$REPO_ROOT
 
 # TODO regenerate BSP if QSYS changes ????
 
@@ -18,7 +43,7 @@ git checkout -b replay-bootloader -t origin/socfpga_v2021.01
 
 ./arch/arm/mach-socfpga/qts-filter.sh cyclone5 ../../../ ../ ./board/terasic/de10-nano/qts/
 
-export CROSS_COMPILE=arm-none-linux-gnueabihf-
+export CROSS_COMPILE=${GCC_PREFIX}
 make socfpga_de10_nano_defconfig
 make -j 48
 
